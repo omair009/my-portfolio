d92923ef3be8a18ebc636f5a220cde326ce6aecc
Mohammed Omairuddin <117035500+omair009@users.noreply.github.com>
2023-02-20 22:04:25 +0530

    Add files via upload

---
+++ Portfolio3/script.js
@@ -0,0 +1,16 @@
+// let x = document.getElementById('header')
+// console.log(x)
+
+//     document.body.addEventListener('click', () => {
+//         x.style.zIndex = '-1'
+//         x.style.position = 'fixed'
+//     })
+
+// let y = document.getElementsByTagName('main')[0]
+// console.log(y)
+// y.style.visibility = 'none'
+
+// let z = document.getElementById('scroll')
+// z.addEventListener('onclick', ()=>{
+//     y.style.visibility = true
+// })
